0ee5ae246e4f8e8ff8ae237804fe7118a1029460
aliia <aliia.malaeva@alatoo.edu.kg>
2025-11-14 18:13:56 +0600

    base commit

---
+++ src/main/java/kg/kicb/users_phones/UsersPhonesApplication.java
@@ -0,0 +1,13 @@
+package kg.kicb.users_phones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersPhonesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersPhonesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/kicb/users_phones/UsersPhonesApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kicb.users_phones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersPhonesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
